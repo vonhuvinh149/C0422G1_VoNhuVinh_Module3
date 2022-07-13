3d5ea91c52f87a6b9f87190487bab11ca1f721f7
vonhuvinh149 <vonhuvinh149@gmail.com>
2022-07-13 15:14:39 +0700

    casestudy

---
+++ case_study/case_study_1.sql
@@ -0,0 +1,115 @@
+create database castydy_furama;
+
+use castydy_furama;
+
+CREATE TABLE nhan_vien (
+    ma_nhanvien INT AUTO_INCREMENT,
+    ho_ten VARCHAR(45),
+    ngay_sinh DATE,
+    so_cmnd VARCHAR(45),
+    luong DOUBLE,
+    so_dien_thoai VARCHAR(45),
+    email VARCHAR(45),
+    dia_chi VARCHAR(45),
+    ma_vi_tri INT,
+    ma_trinh_do INT,
+    ma_bo_phan INT,
+    PRIMARY KEY (ma_nhanvien)
+);
+
+
+
+CREATE TABLE khach_hang (
+    ma_khach_hang INT AUTO_INCREMENT,
+    ma_loai_khach INT,
+    ho_va_ten VARCHAR(45),
+    ngay_sinh DATE,
+    gioi_tinh BIT(1),
+    so_cmnd VARCHAR(45),
+    so_dien_thoai VARCHAR(45),
+    email VARCHAR(45),
+    dia_chi VARCHAR(45),
+    PRIMARY KEY (ma_khach_hang)
+);
+
+CREATE TABLE hop_dong (
+    ma_hop_dong INT AUTO_INCREMENT,
+    ngay_lam_hop_dong DATETIME,
+    ngay_ket_thuc DATETIME,
+    tien_dat_coc DOUBLE,
+    ma_nhan_vien INT,
+    ma_khach_hang INT,
+    ma_dich_vu INT,
+    PRIMARY KEY (ma_hop_dong)
+);
+
+CREATE TABLE dich_vu (
+    ma_dich_vu INT AUTO_INCREMENT,
+    ten_dich_vu VARCHAR(45),
+    dien_tinh INT,
+    chi_phi_thue DOUBLE,
+    s_nguoi_toi_da INT,
+    ma_kieu_thue INT,
+    ma_loai_dic_vu INT,
+    tieu_chuan_phong VARCHAR(45),
+    mo_ta_tien_nghi_khac VARCHAR(45),
+    dien_tich_ho_boi DOUBLE,
+    so_tang INT,
+    dich_vu_mien_phi_di_kem TEXT,
+    PRIMARY KEY (ma_dich_vu)
+);
+
+CREATE TABLE vi_tri (
+    ma_vi_tri INT,
+    ten_vi_tri VARCHAR(45),
+    PRIMARY KEY (ma_vi_tri)
+);
+
+CREATE TABLE trinh_do (
+    ma_trinh_do INT,
+    ten_trinh_do VARCHAR(45),
+    PRIMARY KEY (ma_trinh_do)
+);
+
+CREATE TABLE bo_phan (
+    ma_bo_phan INT,
+    ten_bo_phan VARCHAR(45),
+    PRIMARY KEY (ma_bo_phan)
+);
+
+
+CREATE TABLE loai_khach (
+    ma_loai_khach INT,
+    ten_loai_khach VARCHAR(45),
+    PRIMARY KEY (ma_loai_khach)
+);
+
+CREATE TABLE loai_dich_vu (
+    ma_loai_dich_vu INT,
+    ten_loai_dich_vu VARCHAR(45),
+    PRIMARY KEY (ma_loai_dich_vu)
+);
+
+CREATE TABLE ma_dich_vu_di_kem (
+    ma_dich_vu_di_kem INT,
+    teb_dich_vu_di_kem VARCHAR(45),
+    gia DOUBLE,
+    don_vi VARCHAR(10),
+    trang_thai VARCHAR(45),
+    PRIMARY KEY (ma_dich_vu_di_kem)
+);
+
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT,
+    ma_hop_dong INT,
+    ma_dich_vu_di_kem INT,
+    so_luong INT,
+    PRIMARY KEY (ma_hop_dong_chi_tiet)
+);
+
+
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT,
+    tem_kieu_thue VARCHAR(45),
+    PRIMARY KEY (ma_kieu_thue)
+);
